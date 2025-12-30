946b236051f5556724fabe3d6a841ab6720e3fc5
SooAh0208 <santacoyaki11@gmail.com>
2025-12-30 18:02:38 +0900

    router폴더생성

---
+++ src/router/index.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Page1 from "@pages/Page1.jsx"
+import Page2 from "@pages/Page2.jsx"
+
+const Router = () =>{
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component:Page1,
+  },
+   {
+    path: "/page2",
+    Component:Page2,
+  }
+]);
+  return (
+    <>
+      <RouterProvider router={router} />,
+    </>
+  )
+}
+  export default Router
