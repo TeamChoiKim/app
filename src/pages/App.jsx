3f419c1672f6d6f497e4a65e6ece7748aef3d201
YoungYun1181 <youngyun1181@gmail.com>
2025-12-31 10:15:09 +0900

    app

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,25 +1,11 @@
 import '@styles/App.css'
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
-import Page1 from "@pages/Page1.jsx"
-import Page2 from "@pages/Page2.jsx"
+import Router from '@router'
+
 
 function App() {
-  const router = createBrowserRouter([
-  {
-    path: "/",
-    Component:Page1,
-  },
-   {
-    path: "/page2",
-    Component:Page2,
-  },
-]);
+
   return (
-    <>
-      <RouterProvider router={router} />,
-    </>
+      <Router />
   )
 }
 
