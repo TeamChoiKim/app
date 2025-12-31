14aa5fb2ad240689a3481a82b7534dd853b0365e
choegayoung <choega6614@naver.com>
2025-12-31 09:50:58 +0900

    route다른버전

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,23 +1,32 @@
-import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
-import Page1 from "@pages/Page1.jsx"
-import Page2 from "@pages/Page2.jsx"
+//import { createBrowserRouter } from "react-router";
+//import { RouterProvider } from "react-router/dom";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Page1 from '@pages/Page1.jsx';
+import Page2 from '@pages/Page2.jsx';
 
-const Router = () =>{
-const router = createBrowserRouter([
+
+
+const Router = () => {
+  /*
+  const router = createBrowserRouter([
   {
     path: "/",
-    Component:Page1,
+    Component: Page1
   },
    {
     path: "/page2",
-    Component:Page2,
-  }
+    Component: Page2
+  },
 ]);
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  )
+  return <RouterProvider router={router} />
+}*/
+return(
+ <BrowserRouter>
+ <Routes>
+  <Route path="/" element={<Page1 />} />
+  <Route path="page2/" element={<Page2 />} />
+  </Routes>
+  </BrowserRouter>
+)
 }
-  export default Router
+export default Router
